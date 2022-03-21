dd2ba4eb4bcddf2b4218d7f4cf7932b568ab6805
Ricard Lado <ricardlado@hotmail.com>
2022-03-21 11:50:10 +0100

    Added argparser; fixed the use of "&"
    - Added command-line arguments to improve usability
    - Added dedicated output file option (to avoid overwritting)
    - Now "&" on the output files are "\&" (avoids errors)
    - Formated the code using autopep8
    - Changed README to reflect the changes

--- bibtex_generator.py
+++ bibtex_generator.py
@@ -4,36 +4,39 @@ from tqdm import tqdm
 from bibtexparser.bwriter import BibTexWriter
 from bibtexparser.bibdatabase import BibDatabase
 from bibtexparser.bparser import BibTexParser
-from bibtexparser.customization import  convert_to_unicode
+from bibtexparser.customization import convert_to_unicode
+import argparse
 
 delete_items = [
-    'issn',
-    'doi',
-    'month',
-    'eprint',
     'abstract',
-    'archiveprefix',
-    'keywords',
-    'file',
-    'eprinttype',
-    'shorttitle',
     'address',
+    'annote',
+    'archiveprefix',
+    'doi',
+    'eprint',
+    'eprinttype',
+    'file',
     'isbn',
+    'issn',
+    'keywords',
     'langid',
+    'month',
     'publisher',
+    'shorttitle'
 ]
 
 
-def replace_content(text,abbr_df):
-    for index,row in abbr_df.iterrows():
-        text = text.replace(f"{row[0]}",f"{row[1]}")
-    
+def replace_content(text, abbr_df):
+    for index, row in abbr_df.iterrows():
+        text = text.replace(f"{row[0]}", f"{row[1]}")
+
     if 'arxiv' in text.lower():
         arxiv_num = "".join(list(filter(str.isdigit, text)))
         text = f"arXiv:{arxiv_num[0:4]}.{arxiv_num[4:]}"
 
     ' '.join(text.split())
     return text
+
 
 def arxiv_check(entry):
     if 'journal' in entry:
@@ -42,35 +45,53 @@ def arxiv_check(entry):
             arxiv_num = "".join(list(filter(str.isdigit, entry['journal'])))
             entry['journal'] = f"arXiv:{arxiv_num[0:4]}.{arxiv_num[4:]}"
             ' '.join(entry['journal'].split())
-            entry['year'] = entry['year']+f". [Online]. Available: http://arxiv.org/abs/{arxiv_num[0:4]}.{arxiv_num[4:]}"
+            entry['year'] = entry['year'] + \
+                f". [Online]. Available: http://arxiv.org/abs/{arxiv_num[0:4]}.{arxiv_num[4:]}"
 
     return entry
 
 
+if __name__ == "__main__":
+    # Argument parser
+    parser = argparse.ArgumentParser(
+        description='Script to convert names of journals and conferences into abbreviations to fit a journal\'s requirements. Common abbreviations were found in the IEEE-reference-guide, but you can customize any abbreviation you want by modifying the abbr_list file! ArXiv articles are treated specially.')
+    parser._action_groups.pop()
+    required = parser.add_argument_group('required arguments')
+    optional = parser.add_argument_group('optional arguments')
 
-if __name__ =="__main__":
+    # Command-line parameters
+    required.add_argument('-i', '--input', type=str,
+                          required=True, help='Path of the input .bib file')
+    optional.add_argument('-o', '--output', type=str, default=None,
+                          help='Output path for the resulting .bib file')
 
+    args = parser.parse_args()
+
+    # Main
     abbr_df = pd.read_excel(r'./abbr_list.xlsx')
-    with open('My Library.bib',encoding='UTF-8') as bibtex_file:
+    with open(args.input, encoding='UTF-8') as bibtex_file:
         parser = BibTexParser(common_strings=True)
         parser.customization = convert_to_unicode
-        bib_database = bibtexparser.load(bibtex_file,parser)
+        bib_database = bibtexparser.load(bibtex_file, parser)
         for entry in tqdm(bib_database.entries):
             for item in delete_items:
                 if item in entry:
                     del entry[item]
             if 'journal' in entry:
-                entry['journal'] = replace_content(entry['journal'],abbr_df)
+                entry['journal'] = replace_content(entry['journal'], abbr_df)
             if 'booktitle' in entry:
-                entry['booktitle'] = replace_content(entry['booktitle'],abbr_df)
-            
+                entry['booktitle'] = replace_content(
+                    entry['booktitle'], abbr_df)
+
             entry = arxiv_check(entry)
 
-
+    # Write the output file
     writer = BibTexWriter()
     writer.indent = '    '
     writer.comma_first = False
-    with open('My Library.bib', 'w') as bibfile:
-        bibfile.write(writer.write(bib_database))
-
-
+    if args.output != None:
+        with open(args.output, 'w') as bibfile:
+            bibfile.write(writer.write(bib_database).replace('&','\&'))
+    else:
+        with open('output.bib', 'w') as bibfile:
+            bibfile.write(writer.write(bib_database).replace('&','\&'))
